24044f8d46417b18e9c03f59ce84962c44cfb7bd
vnkpurohit <vnkpurohit@gmail.com>
2022-05-07 17:55:15 +0530

    test done by vinayak;

---
+++ app/Models/Event.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Workshop;
+
+class Event extends Model
+{
+
+    public function workshops()
+    {
+        return $this->hasMany(Workshop::class);
+    }
+
+
+}
